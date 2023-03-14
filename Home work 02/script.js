0a0127ae7231c177e9d10bd1b3649026837935cd
Olga Yastremska <olga.yastremska.v@gmail.com>
2023-03-14 16:45:14 +0200

    need refactoring

--- Home work 02/script.js
+++ Home work 02/script.js
@@ -1,46 +1,33 @@
 
-let N = +prompt('Введіть число ');
-console.log("перше число N: " + N);
-if (N % 1 == 0) { //перевірка чи ціле число
-}
-else {
-console.log("Не ціле число");
-}
+let N;
+let M;
 
-let M = +prompt('Введіть число ');
+while (true){
+        let n = parseInt(prompt('Введіть перше число N'));
+        let m = parseInt(prompt('Введіть друге число M'));
+        N = n;  
+        M = m;
+        if (Number.isInteger(N) && Number.isInteger(M) &&  N>0 &&  N<M) break;
+}
+console.log("перше число N: " + N);
 console.log("друге число M: " + M);
-if (M % 1 == 0) { //перевірка чи ціле число
-}
-else {
-console.log("Не ціле число");
-}
-const all = confirm("Чи потрібно пропускати парні числа?") // true - так, false - ні
-console.log('пропускати парні числа ' + all);
+
+const question = confirm("Чи потрібно пропускати парні числа?") // true - так, false - ні
+console.log("пропускати парні числа " + question);
 
 let sum = 0;
 
 for (let i = N; i <= M; i++) {
-   if (i % 2 == 0) {//Парні числа потрібно перевіряти 
-     if(all === false) {// Рахувати тільки парні (не пропускати нічого)
-       sum+=i;
+     if (i % 2 == 0) {
+         if (question === false) {
+                        sum += i;
+         }
+     } else {
+                sum += i;
+
      }
-  } else { 
-  sum += i; //Тут непарні числа. Їх всіх рахуєм
-  } 
-}
+};
+
 
 console.log(sum);
 alert(sum);
-
-
-
-
-
-
-
-
-
-
-
-
-
